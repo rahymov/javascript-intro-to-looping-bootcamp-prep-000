742dc8401f1625d405a5b7bf3830244dbc98b79a
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-03-22 19:22:52 +0000

    automatically backed up by learn

--- loops.js
+++
@@ -1,8 +0,0 @@
-function makeArray(){
-  const array = [];
-  const t = Math.floor(Math.random() * 10);
-  
-  for(i=0; i < t ; i++){
-    return `I am ${i} strange loop${i === 0? '' : 't'}`;
-  }
-}
